579f65c664d3d9c9baa976d77f1fe406cc5f5b37
viyv <84920939+viyv@users.noreply.github.com>
2023-03-01 21:00:35 +0800

    Update entrypoint.sh

--- official/entrypoint.sh
+++ official/entrypoint.sh
@@ -4,6 +4,7 @@ start() {
   if [ ! -f /app/jdTools.jar ] && [ -f /app/tools.zip ]; then
     unzip /app/tools.zip -d /app
   fi
+  java -Djava.library.path=prebuilt/linux64
   java -jar jdTools.jar --server.port=80 --app.jd.version=11.0.2
 }
 
